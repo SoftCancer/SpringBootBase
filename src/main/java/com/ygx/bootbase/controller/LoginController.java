d33bdece32d4dba78422ada7c31f5c530b44d418
yaogx <ygxzzu@163.com>
2021-01-08 00:00:23 +0800

    在Controller 上测试 Swagger 接口

--- src/main/java/com/ygx/bootbase/controller/LoginController.java
+++ src/main/java/com/ygx/bootbase/controller/LoginController.java
@@ -2,10 +2,13 @@ package com.ygx.bootbase.controller;
 
 import com.ygx.bootbase.common.configure.ConfigurationServiceUrl;
 import com.ygx.bootbase.common.result.ResultMsg;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -21,22 +24,25 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping("login")
+@Api(value = "登录接口")
 public class LoginController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @RequestMapping("/spring")
-    public ResultMsg login(){
+    @ApiOperation(value = "日志测试接口", notes = "请求路径：login/spring")
+    @RequestMapping(value = "/spring",method = RequestMethod.GET)
+    public ResultMsg login() {
         logger.info("测试日志输出");
-        Map<String,Object> map  = new HashMap<>();
-        map.put("name","姚明");
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "姚明");
         log.info("Slf4j");
         return ResultMsg.success(map);
     }
 
     @Resource
     private ConfigurationServiceUrl microServiceUrl;
-    @RequestMapping("/config")
+
+    @RequestMapping(value = "/config",method = RequestMethod.POST)
     public String testConfig() {
         logger.info("=====获取的订单服务地址为：{}",
                 microServiceUrl.getOrderUrl());
